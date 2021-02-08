21fb8d245016dd685f591c9ccd3dcf3278216a01
signoret <tanguy.signoret@etu.univ-grenoble-alpes.fr>
2021-02-08 11:03:26 +0100

    MAJ Tower

--- Tower.py
+++ Tower.py
@@ -3,16 +3,16 @@ import pygame
 class Tower:
     """
     Tower :\n
-    \tAttibuts :\n
-    - sprite : sprite of the Tower (pygame image)\n
-    - name : name of the fruit or vegetable (not given in the constructor)  (string)\n
-    - rate : fire rate of the tower (int)\n
-    - damage : level of damage of the tower (int)\n
-    - coordinates : coordinates of the tower (tupple of int)\n
-    - energy : level of energy of the tower (int)\n
+    Attibuts :\n
+    \tsprite : sprite of the Tower (pygame image)
+    \tname : name of the fruit or vegetable (not given in the constructor)  (string)
+    \trate : fire rate of the tower (int)
+    \tdamage : level of damage of the tower (int)
+    \tcoordinates : coordinates of the tower (tupple of int)
+    \tenergy : level of energy of the tower (int)
     """
 
-    def __init__(self, sprite, name, rate, damage):
+    def __init__(self, sprite, name, rate, damage, coordinates = (10,10) ):
         """
         Tower : constructor of a Tower\n
         Arguments :\n
@@ -21,16 +21,22 @@ class Tower:
         \tcoordinates : coordinates of the tower (tupple of int)
         \trate : fire rate of the tower (int)
         \tdamage : the level of damage of the tower (int)
+        \coordinates : the coordinates of the tower (tupple of int) (default (0,0))
         """
         self.__sprite = sprite
         self.__name = name
         self.__rate = rate
         self.__damage = damage
-        self.__coordinates = (10,10)
+        self.__coordinates = coordinates
         self.__energy = 1 #100%
     
-    def attack(self, screen):
-        """Pas encore implémenté ..."""
+    def attack(self, screen, direction):
+        """
+        Attack a position\n
+        Parameters :\n
+        \tscreen : the pygame screen
+        \tdirection : the direction to attack
+        """
         pass
 
     def draw(self, screen):
