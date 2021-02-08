c17277b0c069134e4bddafa114c03e24fc438c40
signoret <tanguy.signoret@etu.univ-grenoble-alpes.fr>
2021-02-08 14:54:33 +0100

    J'ai presque fini avec les pip ...

--- View.py
+++ View.py
@@ -3,6 +3,8 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import sys
 import copy
 import pygame
+import math as m
+from Tower import Tower
 
 #Set the default position of the pygame window
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (50, 50)
@@ -31,6 +33,10 @@ class View:
                     elif event.type == pygame.KEYDOWN:
                         if event.key == pygame.K_ESCAPE:
                             self.__crashed = True
+                        elif event.key == pygame.K_SPACE:
+                            for elem in self.__graphic_elements:
+                                if isinstance(elem, Tower):
+                                    self.__graphic_elements.append(elem.attack(m.pi/2))
 
                 mouse_pos = pygame.mouse.get_pos()
                 
