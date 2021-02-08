985aa5d510576ea13c419c08637491ef9d2dd68e
signoret <tanguy.signoret@etu.univ-grenoble-alpes.fr>
2021-02-08 10:45:02 +0100

    Maj de Tower

--- Tower.py
+++ Tower.py
@@ -6,17 +6,17 @@ class Tower:
     \tAttibuts :\n
     - sprite : sprite of the Tower (pygame image)\n
     - name : name of the fruit or vegetable (not given in the constructor)  (string)\n
-    - coordinates : coordinates of the tower (tupple of int)\
     - rate : fire rate of the tower (int)\n
     - damage : level of damage of the tower (int)\n
+    - coordinates : coordinates of the tower (tupple of int)\n
     - energy : level of energy of the tower (int)\n
     """
 
-    def __init__(self, sprite, name, coordinates, rate, damage):
+    def __init__(self, sprite, name, rate, damage):
         """
         Tower : constructor of a Tower\n
         Arguments :\n
-        \tsprite : sprite of the Tower (pygame image)
+        \tsprite : sprite of the Tower (path png)
         \tname : name of the fruit or vegetable (not given in the constructor)  (string)
         \tcoordinates : coordinates of the tower (tupple of int)
         \trate : fire rate of the tower (int)
@@ -24,15 +24,17 @@ class Tower:
         """
         self.__sprite = sprite
         self.__name = name
-        self.__coordinates = (0,0)
         self.__rate = rate
         self.__damage = damage
+        self.__coordinates = (10,10)
         self.__energy = 1 #100%
     
+    def attack(self, screen):
+        """Pas encore implémenté ..."""
+        pass
+
     def draw(self, screen):
         image = pygame.image.load(self.__sprite)
-        tile = image.subsurface(((32, 0), (32, 32)))
+        tile = image.subsurface(((0, 0), (32, 32)))
 
-        for i in range(32):
-            for j in range(24):
-                screen.blit(tile, (32 * i, 32 * j))
+        screen.blit(tile,self.__coordinates)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from View import View
 from Garden import Garden
+from Tower import Tower
 
 def calculations(args):
 
@@ -11,5 +12,8 @@ def update(args, graphic_elements):
 
 #Main
 
+
+#towerTest= Tower('assets/fruits-veggies/Acorn.png','Acorn',1,1)
+#view = View([garden], [garden, towerTest], calculations, update)
 garden = Garden([])
 view = View([garden], [garden], calculations, update)
