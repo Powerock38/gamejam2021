85d9424325531a6cb6fb565011f8f57133e9f656
Joris Martin <joris.martin@etu.univ-grenoble-alpes.fr>
2021-02-08 15:10:27 +0100

    merge

--- main.py
+++ main.py
@@ -43,11 +43,7 @@ garden.spawnEnemy()
 
 towertest = Tower("assets/fruits-veggies/Acorn.png","Acorn",1,1,(5*32,10*32))
 
-<<<<<<< HEAD
 # Create HUD
 hud = HUD(100,10)
-=======
-hud = HUD(100,100)
->>>>>>> c5a79713f9c3d63efe69d5722cc17b69fae22013
 
 view = View([garden], [garden,hud,towertest], calculations, update)
