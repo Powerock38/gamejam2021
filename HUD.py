f4e96335806576294138ac7938a31c4afcaf4196
Joris Martin <joris.martin@etu.univ-grenoble-alpes.fr>
2021-02-09 15:17:29 +0100

    Fix HUD

--- HUD.py
+++ HUD.py
@@ -16,6 +16,16 @@ class HUD:
         
         self.__water_image = pygame.image.load("assets/waterdrop.png")
 
+        self.set_water(water)
+        self.set_level(level)
+
+        # Create the tower text
+        self.__font = self.get_font(25)
+        tower_text = self.__font.render('Towers ', False, Utils.WHITE)
+        self.__surface.blit(tower_text, (18, 87))
+
+
+
         self.__towers_rect = []
         
         for col in range(2):
@@ -35,8 +45,8 @@ class HUD:
                 # Create tower hover
                 hover = pygame.Surface((50,15))
                 hover.fill(Utils.RED)
-                font = self.get_font(10)
-                hover_text = font.render("Id : " + str(Utils.TOWERS[elem_number]["id"]), False, Utils.BLACK)
+                self.__font = self.get_font(10)
+                hover_text = self.__font.render("Id : " + str(Utils.TOWERS[elem_number]["id"]), False, Utils.BLACK)
                 hover.blit(hover_text, (5,5))
 
                 self.__towers_rect.append(
@@ -51,14 +61,14 @@ class HUD:
                 x = 36 + (55 * col)
                 y = 162 + (85 * elem)
                 # Get the size wich will be occupated by the text
-                font = self.get_font(12)
-                dim = font.size(Utils.TOWERS[elem_number]["name"])
+                self.__font = self.get_font(12)
+                dim = self.__font.size(Utils.TOWERS[elem_number]["name"])
 
-                item_text = font.render(Utils.TOWERS[elem_number]["name"], False, Utils.WHITE)
+                item_text = self.__font.render(Utils.TOWERS[elem_number]["name"], False, Utils.WHITE)
                 self.__surface.blit(item_text, (x-(int(dim[0]/2)), y+5))
 
                 # Display price
-                item_text = font.render(str(Utils.TOWERS[elem_number]["price"]), False, Utils.WHITE)
+                item_text = self.__font.render(str(Utils.TOWERS[elem_number]["price"]), False, Utils.WHITE)
                 x -= 10
                 self.__surface.blit(self.__water_image, (x-dim[0]/2, y+dim[1]))
                 self.__surface.blit(item_text, ((x-dim[0]/2) + 32, y+dim[1] + 8))
@@ -73,13 +83,49 @@ class HUD:
         return self.__water
 
     def set_water(self, water):
+
         self.__water = water
+
+        # Initialise font with the font available in assets/font
+        self.__font = self.get_font(25)
+
+        x = int(self.get_surface().get_width() / 2)
+        y = 25
+
+        box = pygame.Surface((self.__surface.get_width(),30))
+        box.fill(Utils.GRAY)
+        self.__surface.blit(box, (0,730))
+
+        # Create the text water
+        self.__font = self.get_font(20)
+        self.__water_text = self.__font.render('Water ' + str(self.get_water()), False, Utils.BLUE)
+
+        dim = self.__font.size("Water : " + str(self.get_water()))
+        self.__surface.blit(self.__water_text, (x - (int((dim[0]) / 2)), 730))
+        self.__surface.blit(self.__water_image, (dim[0] - 10, 730))
 
     def get_level(self):
         return self.__level
 
     def set_level(self, level):
+
         self.__level = level
+
+        # Initialise font with the font available in assets/font
+        self.__font = self.get_font(25)
+
+        # x, y cords
+        x = int(self.get_surface().get_width() / 2)
+        y = 25
+
+        box = pygame.Surface((self.__surface.get_width(),30))
+        box.fill(Utils.GRAY)
+        self.__surface.blit(box, (0,y))
+
+        # Create the level text
+        self.__level_text = self.__font.render('Level ' + str(self.get_level()), False, Utils.WHITE)
+        dim = self.__font.size("Level " + str(self.get_level()))
+        self.__surface.blit(self.__level_text, (x - (int(dim[0] / 2)), y))
 
     def get_surface(self):
         return self.__surface
@@ -92,31 +138,5 @@ class HUD:
 
     # Draw the element
     def draw(self, screen):
-
-
-        # Initialise font with the font available in assets/font
-        font = self.get_font(25)
-
-        # x, y cords
-        x = int(self.get_surface().get_width() / 2)
-        y = 25
-
-        # Create the level text
-        level_text = font.render('Level ' + str(self.get_level()), False, Utils.WHITE)
-        dim = font.size("Level " + str(self.get_level()))
-        self.__surface.blit(level_text, (x - (int(dim[0] / 2)), y))
-
-        # Create the tower text
-        tower_text = font.render('Towers ', False, Utils.WHITE)
-        self.__surface.blit(tower_text, (18, 87))
-
-        # Create the text water
-        font = self.get_font(20)
-        water_text = font.render('Water ' + str(self.get_water()), False, Utils.BLUE)
-
-        dim = font.size("Water : " + str(self.get_water()))
-        self.__surface.blit(water_text, (x - (int((dim[0]) / 2)), 730))
-        self.__surface.blit(self.__water_image, (dim[0] - 10, 730))
-
 
         screen.blit(self.__surface, (896,0))
